26092eb2d8afb5ae6dc38cc11f40a35a4cd91bd8
Swaviman2004 <patraswaviman1@gmail.com>
2025-12-30 14:31:48 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const NotFoundError = require("./middleware/404Handling");
+const ApiError = require("./utils/ApiError")
+const app = express();
+
+
+app.use("/api/v1",require("./router"))
+
+app.get("/", (req, res) => {
+  res.send({msg:'Hello World!'});
+});
+
+app.use("",(req,res,next)=>{
+next(new ApiError(404,"Not Found"))
+})
+
+app.use(NotFoundError)
+
+
+module.exports = app
